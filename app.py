ddda173722e39d4beea8da01421324f4a2ade9ca
Pieter <pieterchristyan7@gmail.com>
2025-08-05 12:21:14 +0000

    fix: remove unecessary function

--- app.py
+++ app.py
@@ -54,15 +54,6 @@ llm_json = llm.with_structured_output(RelevanceGrade)
 # Define RAG node for LangGraph
 ### Nodes
 def retrieve(state):
-    """
-    Retrieve documents from vectorstore
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): New key added to state, documents, that contains retrieved documents
-    """
     print("---RETRIEVE---")
     question = state["question"]
 
@@ -119,65 +110,7 @@ def grade_documents(state):
     }
 
 
-
-
 ### Edges
-
-
-def route_question(state):
-    """
-    Route question to web search or RAG
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        str: Next node to call
-    """
-
-    print("---ROUTE QUESTION---")
-    route_question = llm_json.invoke(
-        [SystemMessage(content=router_instructions)]
-        + [HumanMessage(content=state["question"])]
-    )
-    source = json.loads(route_question.content)["datasource"]
-    if source == "websearch":
-        print("---ROUTE QUESTION TO WEB SEARCH---")
-        return "websearch"
-    elif source == "vectorstore":
-        print("---ROUTE QUESTION TO RAG---")
-        return "vectorstore"
-
-
-def decide_to_generate(state):
-    """
-    Determines whether to generate an answer, or add web search
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        str: Binary decision for next node to call
-    """
-
-    print("---ASSESS GRADED DOCUMENTS---")
-    question = state["question"]
-    web_search = state["web_search"]
-    filtered_documents = state["documents"]
-
-    if web_search == "Yes":
-        # All documents have been filtered check_relevance
-        # We will re-generate a new query
-        print(
-            "---DECISION: NOT ALL DOCUMENTS ARE RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
-        )
-        return "websearch"
-    else:
-        # We have relevant documents, so generate answer
-        print("---DECISION: GENERATE---")
-        return "generate"
-
-
 def grade_generation_v_documents_and_question(state):
     print("---CHECK HALLUCINATIONS---")
     question = state["question"]
